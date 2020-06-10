2760fdcdbe83f10e83e36454dc6a7cd65879d6c6
murakami0703 <47410753+murakami0703@users.noreply.github.com>
2020-06-11 07:12:12 +0900

    りざると

--- GameTemplate/GameTemplate/Game/Result.cpp
+++ GameTemplate/GameTemplate/Game/Result.cpp
@@ -9,42 +9,47 @@ Result::Result()
 			std::abort();//すでに出ているためクラッシュ
 		}
 	m_instance = this;
-
 	//リザルト
 	{
 		//0番→R
 		m_itemSprite = g_goMgr->NewGameObject<SpriteRender>();
 		m_itemSprite->Init(L"Assets/sprite/Result_R.dds", 220.0f, 760.0f);
-		m_itemSprite->SetPosition({ -500.0f,0.0f,0.0f });
+		m_itemSprite->SetPosition({ -m_startXPos3,m_startYPos,0.0f });
+		m_itemSprite->SetAlpha(0.0f);
 		m_spriteRender.push_back(m_itemSprite);
 
 		//1番→e
 		m_itemSprite = g_goMgr->NewGameObject<SpriteRender>();
 		m_itemSprite->Init(L"Assets/sprite/Result_e.dds", 200.0f, 760.0f);
-		m_itemSprite->SetPosition({ -300.0f,0.0f,0.0f });
+		m_itemSprite->SetPosition({ -m_startXPos2,m_startYPos,0.0f });
+		m_itemSprite->SetAlpha(0.0f);
 		m_spriteRender.push_back(m_itemSprite);
 
 		//2番→s
 		m_itemSprite = g_goMgr->NewGameObject<SpriteRender>();
 		m_itemSprite->Init(L"Assets/sprite/Result_s.dds", 210.0f, 760.0f);
-		m_itemSprite->SetPosition({ -100.0f,0.0f,0.0f });
+		m_itemSprite->SetPosition({ -m_startXPos1,m_startYPos,0.0f });
+		m_itemSprite->SetAlpha(0.0f);
 		m_spriteRender.push_back(m_itemSprite);
 
 		//3番→u
 		m_itemSprite = g_goMgr->NewGameObject<SpriteRender>();
 		m_itemSprite->Init(L"Assets/sprite/Result_u.dds", 200.0f, 760.0f);
-		m_itemSprite->SetPosition({ 100.0f,0.0f,0.0f });
+		m_itemSprite->SetPosition({ m_startXPos1,m_startYPos,0.0f });
+		m_itemSprite->SetAlpha(0.0f);
 		m_spriteRender.push_back(m_itemSprite);
 		//2番→l
 		m_itemSprite = g_goMgr->NewGameObject<SpriteRender>();
 		m_itemSprite->Init(L"Assets/sprite/Result_l.dds", 200.0f, 760.0f);
-		m_itemSprite->SetPosition({ 300.0f,0.0f,0.0f });
+		m_itemSprite->SetPosition({ m_startXPos2,m_startYPos,0.0f });
+		m_itemSprite->SetAlpha(0.0f);
 		m_spriteRender.push_back(m_itemSprite);
 
 		//3番→t
 		m_itemSprite = g_goMgr->NewGameObject<SpriteRender>();
 		m_itemSprite->Init(L"Assets/sprite/Result_t.dds", 220.0f, 760.0f);
-		m_itemSprite->SetPosition({ 500.0f,0.0f,0.0f });
+		m_itemSprite->SetPosition({ m_startXPos3,m_startYPos,0.0f });
+		m_itemSprite->SetAlpha(0.0f);
 		m_spriteRender.push_back(m_itemSprite);
 
 	}
@@ -62,12 +67,73 @@ Result::~Result()
 	m_instance = nullptr;
 
 }
-void Result::SpriteBound(SpriteRender& m_sprite)
+void Result::SpriteBound(SpriteRender* m_sprite)
 {
+	CVector3 m_position = m_sprite->GetPosition();			//座標。
+	float alpha = m_sprite->GetAlpha();			//座標。
+
+	if (alpha <= 1.0f) {
+		alpha += (1.0f / 60.0);
+	}
+
+	if (m_position.y <= 0.0f) {
+		m_a = -m_a;
+	}
+	else if (m_position.y >= 250.0f) {
+			m_position.y -= m_a;
+
+	}
+	m_sprite->SetPosition(m_position);
+	m_sprite->SetAlpha(alpha);
+
+}
+void  Result::Bound()
+{
+	//Resultspriteをバウンド表示。
+
+
+}
+void  Result::TopLeft()
+{
+	//Resultspriteを縮小、左上に。
+
+}
+void  Result::Soul()
+{
+	//stageでゲットした魂の数カウント
+}
+void  Result::Total()
+{
+	//合計魂のカウント
+
+}
+void  Result::Jewelry()
+{
+	//ボスが排出した宝石処理
 
 }
 
 void Result::Update()
 {
+	SpriteBound(m_spriteRender[0]);
+	SpriteBound(m_spriteRender[1]);
 
+	switch (m_state)
+	{
+	case Result::Result_Bound:
+		Bound();
+		break;
+	case Result::Result_TopLeft:
+		TopLeft();
+		break;
+	case Result::Result_Soul:
+		Soul();
+		break;
+	case Result::Result_Total:
+		Total();
+		break;
+	case Result::Result_Jewelry:
+		Jewelry();
+		break;
+	}
 }
--- GameTemplate/GameTemplate/Game/Result.h
+++ GameTemplate/GameTemplate/Game/Result.h
@@ -15,27 +15,37 @@ public:
 	void Update();
 
 private:
-	void SpriteBound(SpriteRender& m_sprite);
+	void SpriteBound(SpriteRender* m_sprite);
+
+	void Bound();
+	void TopLeft();
+	void Soul();
+	void Total();
+	void Jewelry();
+
 private:
 	enum eState {
-		Result_Bound,
-		Result_Topleft,
-		Result_
+		Result_Bound,		//ResultspriteをBound表示。
+		Result_TopLeft,		//Resultspriteを縮小、左上に。
+		Result_Soul,		//stageでゲットした魂の数カウント
+		Result_Total,		//合計魂のカウント
+		Result_Jewelry,		//ボスが排出した宝石処理
 
-	};
-	enum eResultSprite {
-		Result_R,
-		Result_e,
-		Result_s,
-		Result_u,
-		Result_l,
-		Result_t
 	};
 private:
 	std::vector<SpriteRender*> m_spriteRender;	//スプライトの動的配列
 	SpriteRender* m_itemSprite;
 
 	FontRender* m_font;
+	
+	//リザルトsprite関連
+	const float m_startYPos = 760.0f;
+	const float m_startXPos1 = 100.0f;
+	const float m_startXPos2 = 300.0f;
+	const float m_startXPos3 = 500.0f;
+	float m_a = (760.0f / 60.0f);
+
+	eState m_state = Result_Bound;
 
 };
 
