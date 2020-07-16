f26f334a18edb43f0707eea18126c3a319243324
murakamimiku <37>
2020-07-16 15:04:22 +0900

    マジックナンバーの整理2

--- GameTemplate/GameTemplate/Game/Slaim.cpp
+++ GameTemplate/GameTemplate/Game/Slaim.cpp
@@ -13,10 +13,34 @@
 /////////////////////////////////////////////////////////
 const float SLAIM_COLLISION_RADIUS = 60.0f;	//スライムのカプセルコリジョンの半径。
 const float SLAIM_COLLISION_HEIGHT = 30.0f;	//スライムのカプセルコリジョンの高さ。
+const float CHARACON_TIME = (1.0f / 60.0f);		//キャラコンの経過時間。
 const int SLAIM_MOVEROTATION_RANGE = 360;	//スライムの移動方向の角度の範囲。
 const int TIMER_INITIAL_VALUE_ZERO = 0;		//タイマーの初期化用の値
-const int TIMER_SET_ZERO = 0;		//タイマーの初期化用の値
-const CVector3 ENEMY_FRONT_VECTOR = { 0.0f, 0.0f, 1.0f };	//エネミーの前ベクトル。
+const int CHANG_DIRECTION_TIME = 120;		//方向転換する時間。
+const int TIMER_SET_ONE = 1;				//方向転換完了時にタイマーに代入する値
+const float SLAIM_FOLLOW_LENGTH = 300.0f;	//追跡を始める距離。
+const CVector3 SRAIM_FRONT_VECTOR = { 0.0f, 0.0f, 1.0f };	//エネミーの前ベクトル。
+const float SRAIM_LOITERING_MOVESPEED = 120.0f;				//徘徊の時の速度。
+
+const float SRAIM_LOITERING_LENGTH = 500.0f;			//徘徊に戻る距離。
+const float SRAIM_PREMOVE_LENGTH = 50.0f;				//予備動作開始距離。
+const float FOLLOW_Y_REMOVAL = 0.0f;					//Y成分の除去。
+const CVector3 SRAIM_STOP_MOVE = { 0.0f, 0.0f, 0.0f };	//移動停止させる。
+const float SRAIM_FOLLOW_MOVESPEED = 250.0f;			//徘徊の時の速度。
+
+const float SRAIM_LOITERING_LENGTH = 500.0f;			//徘徊に戻る距離。
+const float SRAIM_LOITERING_LENGTH = 500.0f;			//徘徊に戻る距離。
+const float SRAIM_LOITERING_LENGTH = 500.0f;			//徘徊に戻る距離。
+
+
+const float SRAIM_DEAD_LENGTH = 100.0f;					//死亡判定の距離
+const CVector3 SPLITSRAIM_SCALE = { 1.5f,1.5f,1.5f };	//分割モデルの拡大率。
+const float EF_SOUL_SET_POSY = 430.0f;						//エフェクト、魂出現時のY軸の高さ。
+const CVector3 SMOKE_SCALE = { 20.0f,20.0f,20.0f };			//煙エフェクトの拡大率。
+const CVector3 YELLOWLIGHT_SCALE = { 10.0f,10.0f,10.0f };	//光エフェクトの拡大率。
+
+const float INTERPOLATE_TIME = 0.5f;					//補間時間。
+
 Slaim::Slaim()
 {
 }
@@ -30,6 +54,7 @@ Slaim::~Slaim()
 bool Slaim::Start()
 {
 	//アニメーションクリップのロードとループフラグの設定。
+	//通常モデルのアニメーション
 	m_animClips[eAnimation_Idle].Load(L"Assets/animData/slaim/slaim_idle.tka");
 	m_animClips[eAnimation_Idle].SetLoopFlag(true);
 	m_animClips[eAnimation_Walk].Load(L"Assets/animData/slaim/slaim_walk.tka");
@@ -39,7 +64,7 @@ bool Slaim::Start()
 	m_animClips[eAnimation_Attack].Load(L"Assets/animData/slaim/slaim_attack.tka");
 	m_animClips[eAnimation_Vertigo].Load(L"Assets/animData/slaim/slaim_vertigo.tka");
 	m_animClips[eAnimation_Vertigo].SetLoopFlag(true);
-
+	//分割モデルのアニメーション
 	m_animClips2[eAnimation2_Death].Load(L"Assets/animData/slaim/slaim_dead.tka");
 
 	//cmoファイルの読み込み。
@@ -64,9 +89,9 @@ bool Slaim::Start()
 void Slaim::Idle()
 {
 	//待機。
-	timer++;
+	m_timer++;
 	//一定時間経つと徘徊します。
-	if (timer >= m_idleTime) {
+	if (m_timer >= m_idleTime) {
 		m_timer = TIMER_INITIAL_VALUE_ZERO;
 		m_state = eState_Loitering;
 	}
@@ -75,19 +100,15 @@ void Slaim::Idle()
 void Slaim::Loitering()
 {
 	//徘徊。
-	CVector3 diff = m_toPlayerVec;
-
-	//一定時間ごとに方向転換する。
+	//タイマーが0の時ランダムで方向を決定して回転させる
 	if (m_timer == TIMER_INITIAL_VALUE_ZERO) {
-		//タイマーが0の時ランダムで方向を決定して回転させる
 		m_randRot = rand() % SLAIM_MOVEROTATION_RANGE;
 		m_rotation.SetRotation(CVector3::AxisY(), (float)m_randRot);
-		m_enemyForward = ENEMY_FRONT_VECTOR;
+		m_enemyForward = SRAIM_FRONT_VECTOR;
 		m_rotation.Multiply(m_enemyForward);
-		m_timer = 1;
-		flag = false;
+		m_timer = TIMER_SET_ONE;
 	}
-	else if (m_timer > m_randTimer) {
+	else if (m_timer > CHANG_DIRECTION_TIME) {
 		//タイマーを0に戻す。
 		m_timer = TIMER_INITIAL_VALUE_ZERO;
 	}
@@ -95,8 +116,8 @@ void Slaim::Loitering()
 		m_timer++;
 	}
 
-	//BPが空いてて、かつ距離が近いと追跡状態へ。
-	if (diff.Length() < m_followLength) {
+	//距離が近く、BPが空いていると追跡状態へ。
+	if (m_toPlayerVec.Length() < SLAIM_FOLLOW_LENGTH) {
 		m_battlePoint = SiegePoint::GetInstance()->TryGetBattlePoint(m_position);
 		//空いてるバトルポイントに向かっていくぅ
 		if (m_battlePoint != nullptr) {
@@ -104,99 +125,97 @@ void Slaim::Loitering()
 			m_state = eState_Follow;
 		}
 	}
-	moveVec = m_enemyForward * m_loiteringSpeed;
-	m_position = m_characon.Execute(m_caraTime, moveVec);
-	m_enemyModelRender->PlayAnimation(1);
+	m_moveVec = m_enemyForward * SRAIM_LOITERING_MOVESPEED;
+	m_position = m_characon.Execute(CHARACON_TIME, m_moveVec);
+	m_enemyModelRender->PlayAnimation(eAnimation_Walk);
 
 }
 void Slaim::Follow()
 {	
 	//追尾ちゅ
+	//BPと自分との距離を調べる
 	CVector3 m_toBPVec = m_battlePoint->position - m_position;
 	//バトルポイントに向かって移動しますぅ
-	if (m_toBPVec.Length() > m_toBPPos) {
-		m_toBPVec.y = 0.0f;
+	if (m_toBPVec.Length() > SRAIM_PREMOVE_LENGTH) {
+		m_toBPVec.y = FOLLOW_Y_REMOVAL;
 		m_toBPVec.Normalize();
 	}
 	else {
-		//近いので移動無し
-		m_toBPVec = {0.0f, 0.0f, 0.0f};
+		//近いので停止
+		m_toBPVec = SRAIM_STOP_MOVE;
 	}
 
-	//　向かせたい方向のベクトルを計算する。
-	CVector3 targetVector = m_toPlayerVec;
-	//　Y成分は除去して正規化する。Y成分を入れると空を向いたりするよ。
-	targetVector.y = 0.0f;
-	targetVector.Normalize();
-	m_enemyForward = { 0.0f, 0.0f, 1.0f };
-	CQuaternion qRot;
-	qRot.SetRotation(m_enemyForward, targetVector);
-	m_rotation = qRot;
-
-	moveVec = m_toBPVec * m_followSpeed;
-	m_position = m_characon.Execute(m_caraTime, moveVec);
 	//バトルポイントが決まってる！
 	if (m_battlePoint != nullptr) {
 		//距離が近いので、予備動作へ。
-		if (m_toPlayerVec.Length() <= m_toBPPos) {
+		if (m_toPlayerVec.Length() <= SRAIM_PREMOVE_LENGTH) {
 			m_state = eState_Premove;
 		}
 	}
 	//距離が遠いので、徘徊に戻る。
-	if (m_toPlayerVec.Length() > m_loiteringLength) {
+	if (m_toPlayerVec.Length() > SRAIM_LOITERING_LENGTH) {
 		//
 		m_battlePoint = nullptr;
 		m_state = eState_Loitering;
 	}
 
-	m_enemyModelRender->PlayAnimation(1);
+	//　向かせたい方向のベクトルを計算する。
+	CVector3 targetVector = m_toPlayerVec;
+	//　Y成分は除去して正規化する。Y成分を入れると空を向いたりするよ。
+	targetVector.y = FOLLOW_Y_REMOVAL;
+	targetVector.Normalize();
+	m_enemyForward = SRAIM_FRONT_VECTOR;
+	CQuaternion qRot;
+	qRot.SetRotation(m_enemyForward, targetVector);
+	m_rotation = qRot;
+
+	m_moveVec = m_toBPVec * SRAIM_FOLLOW_MOVESPEED;
+	m_position = m_characon.Execute(CHARACON_TIME, m_moveVec);
+	m_enemyModelRender->PlayAnimation(eAnimation_Walk);
 }
 void Slaim::Premove()
 {
 	//予備動作その1
-	m_enemyModelRender->PlayAnimation(2);
+	m_enemyModelRender->PlayAnimation(eAnimation_Premove);
 	if (m_enemyModelRender->IsPlayingAnimation() == false) {
 		m_state = eState_Premove2;
-
 	}
 }
 void Slaim::Premove2()
 {
-	//予備動作その1
-	m_enemyModelRender->PlayAnimation(3,0.5f);
+	//予備動作その2。
+	m_enemyModelRender->PlayAnimation(eAnimation_Premove2, INTERPOLATE_TIME);
 
 	if (m_enemyModelRender->IsPlayingAnimation() == false) {
 		//プレイヤーの上に離れまーーーーース
-		dddd.Normalize();
-		moveVec = dddd * 4000.0f ;
+		m_setMoveVec.Normalize();
+		m_moveVec = m_setMoveVec * 4000.0f ;
 
 		//だいぶ上に行ったので攻撃しますぅ。
 		if (m_position.y >= 1000.0f) {
 			Player* player = Player::GetInstance();
 			CVector3 P_Position = player->GetPosition();
-			//P_Position.y += 500.0f;
-			dddd = P_Position - m_position;
-
-
+			m_setMoveVec = P_Position - m_position;
+			//攻撃状態に遷移。
 			m_state = eState_Attack;
 		}
 	}
 	else {
+		//プレイヤーとの距離を調べ上昇地点を決めるます。
 		Player* player = Player::GetInstance();
 		CVector3 P_Position = player->GetPosition();
 		P_Position.y += 500.0f;
-		dddd = P_Position - m_position;
+		m_setMoveVec = P_Position - m_position;
 
 	}
-	m_position = m_characon.Execute(m_caraTime, moveVec);
-
+	m_position = m_characon.Execute(CHARACON_TIME, m_moveVec);
 }
 
 void Slaim::Attack()
 {
 
 	if (m_position.y >= 450.0f) {
-		dddd.y -= 400.0f;
+		m_setMoveVec.y -= 400.0f;
 		m_enemyModelRender->PlayAnimation(0);
 	}
 	else {
@@ -214,43 +233,57 @@ void Slaim::Attack()
 
 		}
 	}
-	m_position = m_characon.Execute(m_caraTime, dddd);
+	m_position = m_characon.Execute(CHARACON_TIME, m_setMoveVec);
 	
 }
 void Slaim::Vertigo()
 {
-	m_enemyModelRender->PlayAnimation(5,0.5f);
-	timer2++;
-	if (timer2 >= 60) {
+	//めまい
+	m_enemyModelRender->PlayAnimation(eAnimation_Vertigo, INTERPOLATE_TIME);
+	m_vertigoTimer++;
+	//一定時間が経ったら追尾状態に遷移。
+	if (m_vertigoTimer >= 60) {
 		m_timer = TIMER_INITIAL_VALUE_ZERO;
-		timer2 = 0;
+		m_vertigoTimer = 0;
 		m_state = eState_Loitering;
 	}
 }
 
-void Slaim::Return()
-{}
 void Slaim::Dead()
 {
-	EffectManager* effect = EffectManager::GetInstance();
-	SoulManager* soul = SoulManager::GetInstance();
-	if (m_modelflag == false) {
+	EffectManager* m_effect = EffectManager::GetInstance();
+	SoulManager* m_soul = SoulManager::GetInstance();
+
+	//分割モデルに差し替え
+	if (m_modelChangeFlag == false) {
 		m_enemyDeadModelRender = g_goMgr->NewGameObject<SkinModelRender>();
 		m_enemyDeadModelRender->Init(L"Assets/modelData/slaim_dead.cmo", m_animClips2, eAnimation2_Num);
 		m_enemyDeadModelRender->SetPosition(m_position);
 		m_enemyDeadModelRender->SetRotation(m_rotation);
-		m_enemyDeadModelRender->SetScale({1.5f,1.5f,1.5f});
+		m_enemyDeadModelRender->SetScale(SPLITSRAIM_SCALE);
 		g_goMgr->DeleteGameObject(m_enemyModelRender);
-		m_modelflag = true;
+		m_modelChangeFlag = true;
 	}
 
-	m_enemyDeadModelRender->PlayAnimation(0);
+	m_enemyDeadModelRender->PlayAnimation(eAnimation2_Death);
+
 	if (m_enemyDeadModelRender->IsPlayingAnimation() == false) {
 		//アニメーションの再生が終わったので消しま
-		//エフェクト再生とSoul出現
-		effect->EffectPlayer(EffectManager::Enemy_Dead, { m_position.x ,420.0f,m_position.z }, { 20.0f,20.0f,20.0f });
-		effect->EffectPlayer(EffectManager::Item_Get, { m_position.x ,430.0f,m_position.z }, { 10.0f,10.0f,10.0f });
-		soul->SoulGenerated({ m_position.x ,430.0f,m_position.z });
+		//エフェクト再生
+		//煙
+		m_effect->EffectPlayer(
+			EffectManager::Enemy_Dead,
+			{ m_position.x , EF_SOUL_SET_POSY, m_position.z },
+			SMOKE_SCALE
+		);
+		//光
+		m_effect->EffectPlayer(
+			EffectManager::Item_Get,
+			{ m_position.x , EF_SOUL_SET_POSY, m_position.z },
+			YELLOWLIGHT_SCALE
+		);
+		//Soul出現
+		m_soul->SoulGenerated({ m_position.x , EF_SOUL_SET_POSY, m_position.z });
 
 		//エネミーの数減らします
 		GameData* m_gamedate = GameData::GetInstance();
@@ -270,7 +303,7 @@ void Slaim::Update()
 
 	//攻撃が当たったので死ぬ。
 	if (Player::GetInstance()->GetAttackflag() == true) {
-		if (m_toPlayerVec.Length() < 100.0f) {
+		if (m_toPlayerVec.Length() < SRAIM_DEAD_LENGTH) {
 			m_state = eState_Dead;
 		}
 	}
@@ -281,28 +314,25 @@ void Slaim::Update()
 		Idle();
 		break;
 	case Slaim::eState_Loitering:
-		Loitering();		//徘徊中
+		Loitering();		//徘徊中。
 		break;
 	case Slaim::eState_Follow:
-		Follow();			//プレイヤーを追跡
+		Follow();			//プレイヤーを追跡。
 		break;
 	case Slaim::eState_Premove:
-		Premove();			//予備動作
+		Premove();			//予備動作。
 		break;
 	case Slaim::eState_Premove2:
-		Premove2();			//予備動作
+		Premove2();			//予備動作2。
 		break;
 	case Slaim::eState_Attack:
-		Attack();			//攻撃
+		Attack();			//攻撃。
 		break;
 	case Slaim::eState_Vertigo:
-		Vertigo();			//めまい
-		break;
-	case Slaim::eState_Return:
-		Return();			//徘徊位置に戻る
+		Vertigo();			//めまい。
 		break;
 	case Slaim::eState_Dead:
-		Dead();				//死
+		Dead();				//死。
 		break;
 	}
 
--- GameTemplate/GameTemplate/Game/Slaim.h
+++ GameTemplate/GameTemplate/Game/Slaim.h
@@ -47,7 +47,6 @@ private:
 		eState_Premove2,		//予備動作。
 		eState_Attack,		//攻撃。
 		eState_Vertigo,		//眩暈。
-		eState_Return,	//徘徊位置に戻る。
 		eState_Dead	//死。
 	};
 
@@ -74,7 +73,6 @@ private:
 	void Premove2();		//予備動作。
 	void Attack();		//攻撃。
 	void Vertigo();	//眩暈。
-	void Return();		//徘徊位置に戻る。
 	void Dead();		//死。
 
 private:
@@ -92,60 +90,31 @@ private:
 private:
 
 	//共用
-	CVector3 moveVec = CVector3().Zero();			//座標。
+	CVector3 m_moveVec = CVector3().Zero();			//座標。
+	CVector3 m_setMoveVec = CVector3().Zero();
 	CVector3 m_playerPos = CVector3().Zero();			//プレイヤーの座標。
 	CVector3 m_toPlayerVec = CVector3().Zero();			//プレイヤーまで伸びているベクトル。
 	CVector3 m_enemyForward = { 0.0f, 0.0f, 1.0f };			//スライムの前ベクトル。
 
 	float m_timer = 0;
-	//待機関連
+	//待機状態
 	float m_idleTime = 30.0f;				//待機時間。
 
-	//徘徊関連
+	//徘徊状態
 	bool m_changeflag = true;				//方向転換。
-	const int m_randTimer = 120;			//方向転換する時間。
 	int m_randRot = 0;						//方向の乱数格納。
-	const float m_followLength = 300.0f;	//追跡を始める距離。
-	const float m_loiteringSpeed = 120.0f;	//徘徊速度。
 
-	//追尾関連
-	const float m_toBPPos = 100.0f;			//予備動作開始距離。
-	const float m_followSpeed = 250.0f;		//徘徊速度。
-	const float m_loiteringLength = 500.0f;	//徘徊に戻る距離。
+	//追尾状態
 
-	bool m_modelflag = false;				//方向転換。
+	bool m_modelChangeFlag = false;				//方向転換。
+	//眩暈状態
+	int m_vertigoTimer = 0;
 
 
-
-
-	int m_moveCount = 0;						//巡回用カウント
-	float m_moveSpeed = 0.6f;					//エネミの移動速度。
-	const float m_follSpeed = 1000.0f;				//追尾中の移動速度。
-	const float m_tuisekiLength = 300.0f;		//追尾する距離。
-	const float m_ReturnLength = 1000.0f;		//徘徊位置に戻る距離。
-
-	bool m_battlePosflag = false;				//ちゃんとBPにいますか？
-	BattlePoint* m_battlePoint = nullptr;		//エネミのバトルポイント先
-	float a = 60.0f;
-	float timer = 0;
-
-
-	float m_caraTime = (1.0f / 60.0f);		//キャラコンの経過時間
-
-	int count = 0;							//移動用カウント
-	int wrandom = 0;						//移動の方向乱数
-	const int randomCount = 120;			//ランダムで移動方向切り替えタイマー
-	const float randomSpeed = 300.0f;			//移動速度
-	bool flag = true;
-	bool flag1 = true;
-	int timer1 = 0;
-	int timer2 = 0;
-	float ko = 0;
-
-	CVector3 dddd= CVector3().Zero();
-	CVector3 walk1move = CVector3().Zero();		// 座標。
 
 	bool EneAttackflag = false;					//攻撃中ですか？
+	bool m_battlePosflag = false;				//ちゃんとBPにいますか？
+	BattlePoint* m_battlePoint = nullptr;		//エネミのバトルポイント先
 
 };
 
