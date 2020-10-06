d820b7b7a8964eef6ccb7f1b33235d82f8fa86f8
murakamimiku <37>
2020-10-06 14:48:44 +0900

    ゴーストの準備

--- GameTemplate/GameTemplate/Game/PhysicsObjectBase.h
+++ GameTemplate/GameTemplate/Game/PhysicsObjectBase.h
@@ -1,15 +1,67 @@
 #pragma once
 
+/// <summary>
+/// 物理オブジェクトのベースクラス。
+/// </summary>
 class PhysicsObjectBase
 {
 public:
-	/*!
-	* @brief	コンストラクタ。
-	*/
+	/// <summary>
+	/// コンストラクタ。
+	/// </summary>
 	PhysicsObjectBase();
-	/*!
-	* @brief	デストラクタ。
-	*/
+
+	/// <summary>
+	/// デストラクタ。
+	/// </summary>
 	~PhysicsObjectBase();
+
+	/// <summary>
+	/// ゴーストオブジェクトを解放。
+	/// </summary>
+	virtual void Release() = 0;
+private:
+	/// <summary>
+	/// 静的物理オブジェクト作成処理の共通処理。
+	/// </summary>
+	/// <param name="pos">座標。</param>
+	/// <param name="rot">回転。</param>
+	virtual void CreateCommon(CVector3 pos, CQuaternion rot) = 0;
+
+public:
+	/*!
+		 * @brief	ボックス形状のゴーストオブジェクトを作成。
+		 *@param[in]	pos			座標。
+		 *@param[in]	rot			回転。
+		 *@param[in]	size		サイズ。
+		 */
+	void CreateBox(CVector3 pos, CQuaternion rot, CVector3 size);
+	/*!
+	 * @brief	カプセル形状のゴーストオブジェクトを作成。
+	 *@param[in]	pos			座標。
+	 *@param[in]	rot			回転。
+	 *@param[in]	radius		カプセルの半径。
+	 *@param[in]	height		カプセルの高さ。
+	 */
+	void CreateCapsule(CVector3 pos, CQuaternion rot, float radius, float height);
+	/*!
+	* @brief	球体形状のゴーストオブジェクトを作成。
+	*@param[in]	pos			座標。
+	*@param[in]	rot			回転。
+	*@param[in]	radius		球体の半径。
+	*/
+	void CreateSphere(CVector3 pos, CQuaternion rot, float radius);
+	/*!
+	* @brief	メッシュ形状のゴーストオブジェクトを作成。
+	*@param[in]	pos					座標。
+	*@param[in]	rot					回転。
+	*@param[in]	skinModelData		スキンモデルデータ。
+	*/
+	//void CreateMesh(CVector3 pos, CQuaternion rot, const SkinModelData& skinModelData);
+	void CreateMesh(CVector3 pos, CQuaternion rot, CVector3 scale, const SkinModel& skinModel);
+	void CreateMesh(CVector3 pos, CQuaternion rot, CVector3 scale, SkinModelRender* skinModelRender);
+protected:
+	std::unique_ptr<ICollider> m_collider;	//!<コライダー。
+
 };
 
